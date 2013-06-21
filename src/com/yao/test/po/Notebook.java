e18c494f4d7c01c3b11e082ee6c8508fdbac9442
ysnlvlqw <ysnlvlqw@msn.com>
2013-06-22 00:15:59 +0800

    新建po测试包

---
+++ src/com/yao/test/po/Notebook.java
@@ -0,0 +1,12 @@
+package com.yao.test.po;
+
+import java.io.Serializable;
+
+public class Notebook implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+}
