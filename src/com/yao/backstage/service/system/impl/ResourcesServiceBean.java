6323bbae569fa0d2cb8ec4f623a2fa75e7c99fa4
ysnlvlqw <ysnlvlqw@msn.com>
2013-06-29 20:46:03 +0800

    ...

--- src/com/yao/backstage/service/system/impl/ResourcesServiceBean.java
+++ src/com/yao/backstage/service/system/impl/ResourcesServiceBean.java
@@ -3,7 +3,6 @@ package com.yao.backstage.service.system.impl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.yao.backstage.model.po.Resources;
 import com.yao.backstage.service.system.ResourcesService;
 import com.yao.dao.jpa.JpaDaoSupport;
 @Service 
--- src/com/yao/test/InitServlet.java
+++ src/com/yao/test/InitServlet.java
@@ -5,8 +5,6 @@ import javax.persistence.EntityManagerFactory;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
